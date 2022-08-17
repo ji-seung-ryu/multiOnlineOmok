e534b9c8335dc363c91f1a81d26c66ba18140aae
ji-seung-ryu <8514199@gmail.com>
2022-08-17 14:43:08 +0900

    commit

--- src/main/java/com/example/moo/controller/login/LoginController.java
+++ src/main/java/com/example/moo/controller/login/LoginController.java
@@ -9,63 +9,52 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.example.moo.config.PasswordEncoder;
-import com.example.moo.controller.MemberDto;
-import com.example.moo.controller.NotFoundException;
 import com.example.moo.service.Member;
-import com.example.moo.service.MemberService;
 
 @Controller
 public class LoginController {
-	
-    private final static Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+
+	private final static Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
 
 	private final LoginNeedService loginNeedService;
-	
-	public LoginController (LoginNeedService loginNeedService) {
-		this.loginNeedService = loginNeedService; 
+
+	public LoginController(LoginNeedService loginNeedService) {
+		this.loginNeedService = loginNeedService;
 	}
-	
+
 	@GetMapping("/login")
-	public String returnLoginPage (LoginForm loginForm) {	
+	public String returnLoginPage(LoginForm loginForm) {
 		return "login";
 	}
-	
-	
+
 	@PostMapping("/login")
-	public String doLogin (@Valid LoginForm loginForm, BindingResult bindingResult, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+	public String doLogin(@Valid LoginForm loginForm, BindingResult bindingResult, HttpServletRequest request,
+			RedirectAttributes redirectAttributes) {
 		if (bindingResult.hasErrors()) {
-			 LOGGER.info("form has error!");
-	         return "login";
-	    }
-		
-		
+			LOGGER.info("form has error!");
+			return "login";
+		}
+
 		try {
-			Member member = this.loginNeedService.doLogin (loginForm.getName(), loginForm.getPassword());
+			Member member = this.loginNeedService.doLogin(loginForm.getName(), loginForm.getPassword());
 			redirectAttributes.addAttribute("name", member.getName());
 			HttpSession session = request.getSession();
 			session.setAttribute(SessionConstants.LOGIN_MEMBER, member);
-			
+
 			return "redirect:/memberList";
 		} catch (NameNotFoundException e) {
 			LOGGER.info("not found, NAME : {}", loginForm.getName());
 			return "login";
 		} catch (WrongPasswordException e) {
 			LOGGER.info("wrong password, NAME : {}", loginForm.getName());
-			return "login";	
+			return "login";
 		}
-		
+
 	}
-		
+
 }
--- src/main/java/com/example/moo/controller/login/LoginNeedService.java
+++ src/main/java/com/example/moo/controller/login/LoginNeedService.java
@@ -11,22 +11,23 @@ import com.example.moo.service.MemberService;
 
 @Component
 public class LoginNeedService {
-    private final static Logger LOGGER = LoggerFactory.getLogger(LoginNeedService.class);
+	private final static Logger LOGGER = LoggerFactory.getLogger(LoginNeedService.class);
 
 	private final MemberService memberService;
 	private final PasswordEncoder passwordEncoder;
-	
-	public LoginNeedService (MemberService memberService, PasswordEncoder passwordEncoder) {
+
+	public LoginNeedService(MemberService memberService, PasswordEncoder passwordEncoder) {
 		this.memberService = memberService;
 		this.passwordEncoder = passwordEncoder;
 
 	}
-	
-	public Member doLogin (String name, String password) throws NameNotFoundException, WrongPasswordException {
+
+	public Member doLogin(String name, String password) throws NameNotFoundException, WrongPasswordException {
 		try {
 			Member member = this.memberService.findMemberByName(name);
-			if (!this.passwordEncoder.matches(password, member.getEncodedPassword())) throw new WrongPasswordException();
-			
+			if (!this.passwordEncoder.matches(password, member.getEncodedPassword()))
+				throw new WrongPasswordException();
+
 			return member;
 		} catch (MemberNotFoundException e) {
 			throw new NameNotFoundException();
